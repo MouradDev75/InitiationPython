294df4a0ae8951aa46e3859efbfdf1c0f973c8aa
Mourad <mmahrane@dawan.fr>
2025-12-09 16:49:10 +0100

    Jour2-Fin

--- 07-Fonctions/exo.py
+++ 07-Fonctions/exo.py
@@ -21,3 +21,32 @@ def moyenne_liste(entiers):
     return s / nb
 
 print(moyenne_liste([1,2]))
+
+#3)
+def table_multiplication(nombre, premier_multiple, dernier_multiple):
+    if nombre < 0 or premier_multiple < 0 or dernier_multiple < 0 or premier_multiple > dernier_multiple:
+        print("Params invalides.......")
+    else:
+        for i in range(premier_multiple, dernier_multiple + 1):
+            print(f"{nombre} x {i} = {nombre * i}")
+
+table_multiplication(11,1,10)
+
+#4)
+
+from mesfonctions import menu,convertir_heures_minutes,convertir_minutes_heures
+
+while True:
+ 
+    choix = menu()
+    if choix == '3':
+        break
+
+    elif choix == '1':
+        convertir_heures_minutes()
+
+    elif choix == '2':
+        convertir_minutes_heures()
+
+    else:
+        print('choix invalide.....')
